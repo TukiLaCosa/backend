227056f95f043ad811bfbf2921a62394c6d90cb7
Anelio Alvarez <anelio272014@gmail.com>
2023-09-25 16:37:30 -0300

    se agrega middleware CORS para permitir las requests del front

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.config.config import settings
 from app.routers.players import players
 from app.routers.games import games
 
 app = FastAPI()
+
+# Allowing requests from other origins (CORS)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mapping api routers
 app.include_router(players.router)
