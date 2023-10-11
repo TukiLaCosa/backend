9e262204934b261f325e02d592ed4bd5f128e775
Ignacio Belitzky <ignabelitzky@gmail.com>
2023-10-11 20:01:30 -0300

    Se hicieron tests para el endpoint de iniciar partida

---
+++ app/tests/game_tests/test_init_of_games.py
@@ -0,0 +1,114 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from app.database.models import Game, Player
+from pony.orm import db_session
+
+client = TestClient(app)
+
+
+@db_session
+def create_test_player(name: str):
+    return Player(name=name)
+
+
+@db_session
+def create_test_game(name, min_players, max_players, password, host_player_id):
+    game_data = {
+        "name": name,
+        "min_players": min_players,
+        "max_players": max_players,
+        "password": password,
+        "host_player_id": host_player_id
+    }
+    return client.post("/games", json=game_data)
+
+
+@db_session
+def cleanup_database():
+    Game.select().delete()
+    Player.select().delete()
+
+
+def test_init_game_successfully():
+    cleanup_database()
+    players = []
+    names = ["Ignacio", "Anelio", "Ezequiel",
+             "Amparo", "Santiago", "Nehuen", "Gabriel"]
+    for name in names:
+        players.append(create_test_player(name=name))
+    game = create_test_game(name="TestGame", min_players=4, max_players=7,
+                            password="secret", host_player_id=players[0].id)
+    for player in players[1:]:
+        game_data = {
+            "player_id": player.id,
+            "password": "secret"
+        }
+        client.patch("/games/join/TestGame", json=game_data)
+    response = client.patch(
+        f"/games/TestGame/init?host_player_id={players[0].id}")
+    assert response.status_code == 200, "El codigo de estado de la respuesta no coincide con el esperado"
+    cleanup_database()
+
+
+def test_init_game_less_min_players():
+    cleanup_database()
+    players = []
+    names = ["Ignacio", "Anelio", "Ezequiel",
+             "Amparo", "Santiago", "Nehuen", "Gabriel"]
+    for name in names:
+        players.append(create_test_player(name=name))
+    game = create_test_game(name="TestGame", min_players=4, max_players=7,
+                            password="secret", host_player_id=players[0].id)
+    for player in players[1:2]:
+        game_data = {
+            "player_id": player.id,
+            "password": "secret"
+        }
+        client.patch("/games/join/TestGame", json=game_data)
+    response = client.patch(
+        f"/games/TestGame/init?host_player_id={players[0].id}")
+    assert response.status_code == 400, "El codigo de estado de la respuesta no es 400 (Bad Request)"
+    cleanup_database()
+
+
+def test_init_game_twice():
+    cleanup_database()
+    players = []
+    names = ["Ignacio", "Anelio", "Ezequiel",
+             "Amparo", "Santiago", "Nehuen", "Gabriel"]
+    for name in names:
+        players.append(create_test_player(name=name))
+    game = create_test_game(name="TestGame", min_players=4, max_players=7,
+                            password="secret", host_player_id=players[0].id)
+    for player in players[1:]:
+        game_data = {
+            "player_id": player.id,
+            "password": "secret"
+        }
+        client.patch("/games/join/TestGame", json=game_data)
+    client.patch(f"/games/TestGame/init?host_player_id={players[0].id}")
+    response = client.patch(
+        f"/games/TestGame/init?host_player_id={players[0].id}")
+    assert response.status_code == 400, "El codigo de estado de la respuesta no es 400 (Bad Request)"
+    cleanup_database()
+
+
+def test_init_game_bad_host_id():
+    cleanup_database()
+    players = []
+    names = ["Ignacio", "Anelio", "Ezequiel",
+             "Amparo", "Santiago", "Nehuen", "Gabriel"]
+    for name in names:
+        players.append(create_test_player(name=name))
+    game = create_test_game(name="TestGame", min_players=4, max_players=7,
+                            password="secret", host_player_id=players[0].id)
+    for player in players[1:]:
+        game_data = {
+            "player_id": player.id,
+            "password": "secret"
+        }
+        client.patch("/games/join/TestGame", json=game_data)
+    response = client.patch(
+        f"/games/TestGame/init?host_player_id={players[0].id + 1}")
+    assert response.status_code == 400, "El codigo de estado de la respuesta no es 400 (Bad Request)"
+    cleanup_database()
