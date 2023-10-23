ca911170bb670e6091a741997c89ad890e13fd0c
Ignacio Belitzky <ignabelitzky@gmail.com>
2023-10-23 20:39:19 -0300

    Intento de fix provisorio

--- app/routers/games/services.py
+++ app/routers/games/services.py
@@ -278,6 +278,11 @@ def play_action_card(game_name: str, play_info: PlayInformation) -> Game:
                                 play_info.objective_player_id,
                                 players_not_eliminated - 1)
         objective_player = find_player_by_id(play_info.objective_player_id)
+
+        # fix provisorio
+        if player.position > objective_player.position:
+            game.turn = objective_player.position
+
         process_flamethrower_card(game, player, card, objective_player)
 
     # Analisis
