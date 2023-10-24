96b09288c622545c32adbc0e0ea656350e9a90b6
Ignacio Belitzky <ignabelitzky@gmail.com>
2023-10-24 15:36:36 -0300

    Se modifica codigo que estaba puesto cuando se jugaba una carta de analisis, y se lo pone en la parte del lanzallamas

--- app/routers/games/services.py
+++ app/routers/games/services.py
@@ -274,6 +274,9 @@ def play_action_card(game_name: str, play_info: PlayInformation) -> Game:
                                 players_not_eliminated - 1)
         objective_player: Player = find_player_by_id(play_info.objective_player_id)
 
+        if game.turn != 0 and objective_player.position < player.position:
+            game.turn = game.turn - 1
+
         process_flamethrower_card(game, player, card, objective_player)
 
     # Analisis
@@ -283,9 +286,6 @@ def play_action_card(game_name: str, play_info: PlayInformation) -> Game:
                                 play_info.objective_player_id,
                                 players_not_eliminated - 1)
         objective_player: Player = find_player_by_id(play_info.objective_player_id)
-
-        if game.turn != 0 and objective_player.position < player.position:
-            game.turn = game.turn - 1
         
         # Armo listado de cartas del jugador objetivo para enviar en el body response
         result = process_analysis_card(game, player, card, objective_player)
