211161bad47dad351dbe5e8c683ce7acc7fd8c39
Ignacio Belitzky <ignabelitzky@gmail.com>
2023-10-27 15:39:40 -0300

    Cambio menor

--- app/routers/websockets/services.py
+++ app/routers/websockets/services.py
@@ -12,8 +12,8 @@ async def send_event_cheat_used(player_id: int):
 async def send_list_of_cheats(player_id: int):
     cheats: list[str] = []
     cheats.append(
-        'lz or lanzallamas or flamethrower: Obtienes una carta lanzallamas')
-    cheats.append('ws or whiskey or whisky: Obtienes una carta whiskey')
+        '[lz | lanzallamas | flamethrower]: Obtienes una carta lanzallamas')
+    cheats.append('[ws | whiskey | whisky]: Obtienes una carta whiskey')
     for c in cheats:
         await player_connections.send_message(player_id, 'Loki', c)
 
@@ -27,12 +27,15 @@ async def handle_message(data, player_id):
     for i in players:
         if i != player_id:
             await player_connections.send_message(player_id=i, message_from=message_from, message=message)
+
     if message == 'cheats':
         await send_list_of_cheats(player_id)
-    if message == 'lz' or message == 'lanzallamas' or message == 'flamethrower':
+
+    elif message == 'lz' or message == 'lanzallamas' or message == 'flamethrower':
         flamethrower_cheat(game_name, player_id)
         await send_event_cheat_used(player_id)
-    if message == 'ws' or message == 'whisky' or message == 'whiskey':
+
+    elif message == 'ws' or message == 'whisky' or message == 'whiskey':
         whiskey_cheat(game_name, player_id)
         await send_event_cheat_used(player_id)
 
