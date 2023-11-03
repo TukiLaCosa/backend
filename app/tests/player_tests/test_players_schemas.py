294d66f2a5b4803dc02e8208c743198302e989e2
ezeluduena <ezeluduena123@gmail.com>
2023-11-02 22:10:14 -0300

    minifix

--- app/tests/player_tests/test_players_schemas.py
+++ app/tests/player_tests/test_players_schemas.py
@@ -1,6 +1,5 @@
 from app.routers.players.schemas import PlayerInfo
 from app.routers.cards.schemas import CardResponse
-from typing import List
 
 
 def test_PlayerInfo_schema_was_the_thing_true_success():
@@ -18,7 +17,7 @@ def test_PlayerInfo_schema_was_the_thing_true_success():
     ]
     player = PlayerInfo(name="player", id=1, position=1,
                         rol="THE_THING", hand=cards)
-    assert player.was_the_thing == True, "El jugador debería tener la carta 'La Cosa' en su mano."
+    assert player.was_the_thing == True, "Error en el schema. El jugador debería tener la carta 'La Cosa' en su mano."
 
 
 def test_PlayerInfo_schema_was_the_thing_false_success():
@@ -32,4 +31,4 @@ def test_PlayerInfo_schema_was_the_thing_false_success():
     ]
     player = PlayerInfo(name="player", id=1, position=1,
                         rol="THE_THING", hand=cards)
-    assert player.was_the_thing == False, "El jugador no debería tener la carta 'La Cosa' en su mano."
+    assert player.was_the_thing == False, "Error en el schema. El jugador no debería tener la carta 'La Cosa' en su mano."
